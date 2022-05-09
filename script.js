eb7d0d079ff45ba05995adbafa17c9c94307ff61
mathrl <matheusleite_github@fastmail.com>
2022-05-09 16:31:25 -0300

    .

--- script.js
+++ script.js
@@ -1,54 +1,67 @@
 
+const { createApp } = Vue;
 
-    const { createApp } = Vue;
+createApp({
+  async created() {
+    this.fetchCat();
+  },
 
-    createApp({
-      async created() {
-        this.fetchCat();
-      },
+  methods: {
+    randomIntFromInterval(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    },
 
-      methods: {
-        randomIntFromInterval(min, max) {
-          return Math.floor(Math.random() * (max - min + 1) + min);
-        },
+    // cat stringify
+    // represent a number in car emoji
+    cs(number) {
+      let catString = "";
+      while (number > 0) {
+        catString += "🐱";
+        number--;
+      }
+      return catString;
+    },
 
-        // cat stringify
-        // represent a number in car emoji
-        cs(number) {
-          let catString = "";
-          while (number > 0) {
-            catString += "🐱";
-            number--;
-          }
-          return catString;
-        },
+    yesOrNo(number){
+      if (number) {
+        return "Yes";
+      }
+      return "No";
+    },
 
-        async fetchCat() {
-          let random = this.randomIntFromInterval(0, breeds.length - 1);
-          let randomBreed = breeds[random];
+    async fetchCat() {
+      this.loading = "Loading cat... 🙀";
+      let random = this.randomIntFromInterval(0, breeds.length - 1);
+      let randomBreed = breeds[random];
 
-          let toFetch =
-            this.endpoint +
-            new URLSearchParams({
-              breed_id: randomBreed,
-              size: 'thumb'
-            });
-          let res = await fetch(toFetch);
-          let cat = await res.json();
+      let toFetch =
+        this.endpoint +
+        new URLSearchParams({
+          breed_id: randomBreed,
+          size: "thumb",
+        });
+      let res = await fetch(toFetch, {
+        headers: new Headers({
+          'x-api-key': API_KEY
+        })
+      });
 
-          this.url = cat[0].url;
-          this.catInfo = cat[0].breeds[0];
+      let cat = await res.json();
 
-          console.log(cat[0].breeds[0]);
-        },
-      },
+      this.url = cat[0].url;
+      this.catInfo = cat[0].breeds[0];
 
-      data() {
-        return {
-          apiKey: "1f9225bc-14b7-4b63-8f78-b5f53a066ab7",
-          endpoint: "https://api.thecatapi.com/v1/images/search?",
-          url: "",
-          catInfo: {},
-        };
-      },
-    }).mount("#app");
+      this.loading = " ";
+      //console.log(cat[0].breeds[0]);
+    },
+  },
+
+  data() {
+    return {
+      endpoint: "https://api.thecatapi.com/v1/images/search?",
+      url: "",
+      catInfo: {},
+      loading: "Loading cat... 🙀"
+    };
+  },
+}).mount("#app");
